d8fb5543d02f3066ce94b590fe2081fe31809e64
Fábio Alves <fabioalves.contacto@gmail.com>
2021-01-14 14:33:51 -0100

    ADD: Movement added

--- src/org/academiadecodigo/koxtiposix/acdefender/Player.java
+++ src/org/academiadecodigo/koxtiposix/acdefender/Player.java
@@ -5,25 +5,32 @@ import org.academiadecodigo.simplegraphics.graphics.Rectangle;
 public class Player {
     private int key = 1;
     private int defaultPos;
-    private Rectangle r;
+    private Rectangle playerChar;
     private int pixelJump = 100;
+    private int playerX = 200;
+    private int playerY = 400;
+    private int x = 0;
 
     public Player() {
-        r = new Rectangle(1000, 400, 32, 32);
-        r.draw();
-        r.fill();
+        playerChar = new Rectangle(playerX, playerY, 32, 32);
+        playerChar.draw();
+        playerChar.fill();
     }
 
 
     public void moveUp() {
-
-        r.translate(0, -pixelJump);
+        if (x != 1) {
+            playerChar.translate(0, -pixelJump);
+            x++;
+        }
 
     }
 
     public void moveDown() {
-
-        r.translate(0, pixelJump);
+        if (x != -1) {
+            playerChar.translate(0, pixelJump);
+            x--;
+        }
 
     }
 }
